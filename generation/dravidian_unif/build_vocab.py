96dd02ca36461e78bd90dc40798b41e066753f45
bhavyajeets <bhavyajeet.singh@ada>
2023-02-18 14:44:34 +0530

    added converter

--- generation/dravidian_unif/build_vocab.py
+++ generation/dravidian_unif/build_vocab.py
@@ -5,22 +5,15 @@ from utils import get_text_in_unified_script, get_language_normalizer
 
 normalizer = get_language_normalizer()
 
-langlist  = {'as':[],'bn':[],'en':[],'gu':[] ,'hi':[],'kn':[],'ma':[],'mr':[],'or':[],'pa':[],'ta':[],'te':[]}
+langlist  = {'as':[],'bn':[],'en':[],'gu':[] ,'hi':[],'kn':[],'ml':[],'mr':[],'or':[],'pa':[],'ta':[],'te':[]}
+langlist  = {'as':[],'bn':[],'gu':[] ,'hi':[],'kn':[],'ml':[],'mr':[],'or':[],'pa':[],'ta':[],'te':[]}
 
 tokenizer = MT5Tokenizer.from_pretrained('google/mt5-small', cache_dir="/tmp/hugginface")
 
-target_lang = 'en'
+
 
 for lang in langlist:
-    tot_len = 0
-    fin_number = 0
-
-    print ("========", lang ,"========")
-   
-    pretok = {1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[],10:[]}
     preover = []
-    postover = []
-    postok = {1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[],10:[]}
     read_file = open('/scratch/XAlign/datasets/split_data/'+lang + '/train.jsonl').readlines()
     for line in read_file:
 
@@ -28,18 +21,42 @@ for lang in langlist:
         number = len(line['facts_list'])
         sentence = line['sentence']
         lang_this = line['lang']
-        uni_sentence = get_text_in_unified_script(sentence,normalizer[lang_this],lang_this,target_lang)
         
         tokenzier_args = {'text': sentence, 'truncation': True, 'pad_to_max_length': False, 
                                     'max_length': 1024, 'return_attention_mask': True}
         tokenized_data = tokenizer.encode_plus(**tokenzier_args)
-        print (tokenized_data)
-        prenum = len(tokenized_data['input_ids'])
-        pretok[number].append(prenum)
-        preover.append(prenum)
-        
-    for i in range(1,11):
-        print ("pre ",i, " ", int(sum(pretok[i])/len(pretok[i])), end = ' ')
-        print ( int(sum(postok[i])/len(postok[i])) )
-    print ("pre ", int(sum(preover)/len(preover) ) )
-    print ("post ", int(sum(postover)/len(postover) )) 
+        preover += list(set(tokenized_data['input_ids']))
+
+    preover = set(preover)
+    langlist[lang] = preover
+
+for target_lang in langlist:
+    print ('============',target_lang,'============')
+    gnum = 0
+    for lang in langlist:
+        postover = []
+        read_file = open('/scratch/XAlign/datasets/split_data/'+lang + '/train.jsonl').readlines()
+        for line in read_file:
+
+            line = json.loads(line.strip())
+            number = len(line['facts_list'])
+            sentence = line['sentence']
+            lang_this = line['lang']
+            uni_sentence = get_text_in_unified_script(sentence,normalizer[lang_this],lang_this,target_lang)
+            
+
+            uni_tokenzier_args = {'text': uni_sentence, 'truncation': True, 'pad_to_max_length': False, 
+                                        'max_length': 1024, 'return_attention_mask': True}
+            uni_tokenized_data = tokenizer.encode_plus(**uni_tokenzier_args)
+            postover += list(set(uni_tokenized_data['input_ids']))
+
+
+        postover = set(postover)
+
+        gnum += len(postover.intersection(langlist[target_lang])) /len(postover.union(langlist[target_lang])) 
+        print (lang, "overlap:", len(postover.intersection(langlist[target_lang])) /len(postover.union(langlist[target_lang])), "overlap:", len(postover.intersection(langlist[target_lang])), "after uni:",len (postover), "preuni:", len(langlist[lang]) )
+
+    print (gnum/len(langlist))
+
+
+
