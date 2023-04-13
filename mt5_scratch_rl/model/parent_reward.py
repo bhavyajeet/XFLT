53b48039ac14c348be835ddf34ed940bbd4aa1c5
aditya-hari <adityahari3791@gmail.com>
2023-04-14 04:26:56 +0530

    Fixed parent reward

--- mt5_scratch_rl/model/parent_reward.py
+++ mt5_scratch_rl/model/parent_reward.py
@@ -8,24 +8,28 @@ from functools import lru_cache
 from nltk.util import ngrams 
 from collections import Counter
 import os 
+from icecream import ic 
 
 os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
 def group_duplicates(embeddings, lst, mean=True):
-    embeddings = torch.cat([e.reshape(1, -1) for i, e in enumerate(embeddings) if lst[i]!=None], dim=0)
-    lst = [i for i in lst if i!=None]
-    output = [None for _ in range(len(set(lst)))]
+    embeddings = torch.cat([e.reshape(1, -1) for i, e in enumerate(embeddings) if lst[i] is not None], dim=0)
+    lst = [i for i in lst if i is not None]
+    output = {key: None for key in (set(lst))}
+    if(list(set(lst)) != list(range(max(lst)+1))):
+        ic(f"Group duplicates problem {lst}")
+    output_list = [] 
     i = 0
     for idx, i in enumerate(lst):
-        if(i!=None):
+        if(i is not None):
             if(output[i] == None):
                 output[i] = embeddings[idx, :].reshape(1, -1)
             else:
                 output[i] = torch.cat((output[i], embeddings[idx, :].reshape(1, -1)), dim=0)
     if(mean):
-        for idx, val in enumerate(output):
-            output[idx] = torch.mean(output[idx], dim=0).reshape(1, -1)
-    return output
+        for key, val in output.items():
+            output_list.append(torch.mean(val, dim=0).reshape(1, -1))
+    return output_list
 
 def get_facts(source, token_split=True):
     words = source.split(":")[1].split(" ")
@@ -56,33 +60,41 @@ def entailment_prob(fact_embeddings, generated_ngram, threshold=None):
     return np.mean(torch.max(similarities, dim=1).values.cpu().numpy())
 
 def get_coverage_reward(generated, source,  model, parentTokenizer, parent_device):
-    generated = list(map(lambda gen: re.sub(r"[',.।()]", '', gen), generated))
-    generated_tokens = tuple(map(lambda gen: tuple(re.sub(r'[ ]{2,}', ' ', gen.strip()).split()),  generated))
-    generated_embeddings, g_idx = get_embedding(generated_tokens, model, parentTokenizer, split_into_words=True, parent_device=parent_device)
-    gen_emb = list(map(lambda ge, gi: group_duplicates(ge, gi), generated_embeddings, g_idx))
-    # gen_emb = [] 
-    # for ge, gi in zip(generated_embeddings, g_idx):
-    #     gen_emb.append(group_duplicates(ge, gi))
+    try:
+        generated = list(map(lambda gen: re.sub(r"[',.।()]", '', gen), generated))
+        generated_tokens = tuple(map(lambda gen: tuple(re.sub(r'[ ]{2,}', ' ', gen.strip()).split()),  generated))
+        generated_embeddings, g_idx = get_embedding(generated_tokens, model, parentTokenizer, split_into_words=True, parent_device=parent_device)
+        gen_emb = list(map(lambda ge, gi: group_duplicates(ge, gi), generated_embeddings, g_idx))
+        # gen_emb = [] 
+        # for ge, gi in zip(generated_embeddings, g_idx):
+        #     gen_emb.append(group_duplicates(ge, gi))
 
-    fact_pos_pairs = tuple(map(lambda src: get_facts(src, token_split=True), source))
-    facts = tuple(fact_pos_pairs[i][0] for i in range(len(fact_pos_pairs)))
-    #print(facts)
-    fact_pos = tuple(fact_pos_pairs[i][1] for i in range(len(fact_pos_pairs)))
-    fact_embeddings, f_idx = get_embedding(facts, model, parentTokenizer, split_into_words=True, parent_device=parent_device)
-    #print(fact_embeddings[0].shape)
-    #print(f_idx[0])
-    fact_embeddings = list(map(lambda fe, fi: group_duplicates(fe, fi), fact_embeddings, f_idx))
-    fact_emb = [torch.cat(fe).squeeze() for fe in fact_embeddings]
-    batch_eps = []
-    for generated_tokens_, gen_emb_, fact_emb_ in zip(generated_tokens, gen_emb, fact_emb):
-        g_dict = {t: emb for t, emb in zip(generated_tokens_, gen_emb_)}
-        generated_ngrams = Counter(list(ngrams(generated_tokens_, 1)))
-        eps = [] 
-        for ngram, count in generated_ngrams.items():
-            ngram_embedding = torch.cat([g_dict[i] for i in ngram if i in g_dict]).squeeze()
-            ep = entailment_prob(fact_emb_, ngram_embedding, 0.4)
-            eps.append(ep)
-        batch_eps.append(np.mean(eps))
-    return torch.FloatTensor(batch_eps)
+        fact_pos_pairs = tuple(map(lambda src: get_facts(src, token_split=True), source))
+        facts = tuple(fact_pos_pairs[i][0] for i in range(len(fact_pos_pairs)))
+        #print(facts)
+        fact_pos = tuple(fact_pos_pairs[i][1] for i in range(len(fact_pos_pairs)))
+        fact_embeddings, f_idx = get_embedding(facts, model, parentTokenizer, split_into_words=True, parent_device=parent_device)
+        #print(fact_embeddings[0].shape)
+        #print(f_idx[0])
+        fact_embeddings = list(map(lambda fe, fi: group_duplicates(fe, fi), fact_embeddings, f_idx))
+        fact_emb = [torch.cat(fe).squeeze() for fe in fact_embeddings]
+        batch_eps = []
+        for generated_tokens_, gen_emb_, fact_emb_ in zip(generated_tokens, gen_emb, fact_emb):
+            g_dict = {t: emb for t, emb in zip(generated_tokens_, gen_emb_)}
+            generated_ngrams = Counter(list(ngrams(generated_tokens_, 1)))
+            eps = [] 
+            for ngram, count in generated_ngrams.items():
+                ngram_embs = [g_dict[i] for i in ngram if i in g_dict]
+                if(ngram_embs != []):
+                    ngram_embedding = torch.cat([g_dict[i] for i in ngram if i in g_dict]).squeeze()
+                    ep = entailment_prob(fact_emb_, ngram_embedding, 0.4)
+                    eps.append(ep)
+                else:
+                    eps.append(0)
+            batch_eps.append(np.mean(eps))
+        return torch.FloatTensor(batch_eps)
+    except Exception:
+        ic(f"Coverage reward problem {generated} {source}")
+        return torch.FloatToensor([0.3 for _ in range(len(generated))])
 
 # def get_coverage_reward(generated, source, model, parentTokenizer, parent_device):
